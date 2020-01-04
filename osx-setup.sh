9d6a27ba79a8d02a7504b9fa049508772ae7d306
mikeydiamonds <mpruitt@dnsfilter.com>
2020-01-04 17:17:42 -0500

    error message update

--- osx-setup.sh
+++ osx-setup.sh
@@ -3,9 +3,9 @@
 # A script to set up a new mac using Homebrew.
 
 # notifications
-function echo_ok { echo -e '\033[1;32m'"$1"'\033[0m'; }
-function echo_warn { echo -e '\033[1;33m'"$1"'\033[0m'; }
-function echo_error  { echo -e '\033[1;31mERROR: '"$1"'\033[0m'; }
+function echo_ok { echo '\033[1;32m'"$1"'\033[0m'; }
+function echo_warn { echo '\033[1;33m'"$1"'\033[0m'; }
+function echo_error  { echo '\033[1;31mERROR: '"$1"'\033[0m'; }
 
 echo_ok "Install starting. You may be asked for your password (for sudo)."
 
